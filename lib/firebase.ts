a0c3b3ea92d7ea834b4eccd4b1530036feb75fec
exxsid <cortezleoanthony5@gmail.com>
2025-04-06 14:30:23 +0800

    integrate firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCidcuDCcCf94nlNCvy0mziub3kEa6mUaA",
+  authDomain: "ias-proj-ee80d.firebaseapp.com",
+  projectId: "ias-proj-ee80d",
+  storageBucket: "ias-proj-ee80d.firebasestorage.app",
+  messagingSenderId: "376389779635",
+  appId: "1:376389779635:web:76a915f732c6c6f44ef0c6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
